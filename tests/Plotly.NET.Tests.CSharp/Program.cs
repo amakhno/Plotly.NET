97394b77821cfcb216ea50ab2f0438a37372129e
Kevin Schneider <schneider.kev@outlook.de>
2021-08-13 13:42:23 +0200

    Add C# test project

---
+++ tests/Plotly.NET.Tests.CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Plotly.NET;
+using Microsoft.FSharp.Core;
+
+namespace Plotly.NET.Tests.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chart.Point<int, int, string>(
+                new int[] { 1 },
+                new int[] { 1 },
+                Labels: new string[] {"soos"},
+                TextPosition: StyleParam.TextPosition.BottomCenter
+            ).Show();
+        }
+    }
+}
